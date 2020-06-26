db9ab1eeddd21d460484c903bf0d9a17d8fae891
Irfan Ahmed <57946377+irfan174@users.noreply.github.com>
2020-06-26 21:12:32 +0600

    add the sql file till complete expense generate feature daywise,monthwise, yearwise
    add the sql file till complete expense generate feature daywise,monthwise, yearwise

---
+++ db_file/expense_report.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 26, 2020 at 05:10 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `expense_report`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
+--
+
+CREATE TABLE `expenses` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `details` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `month` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `year` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `expenses`
+--
+
+INSERT INTO `expenses` (`id`, `details`, `amount`, `date`, `month`, `year`) VALUES
+(1, 'Computer', '100000', '25/06/2020', 'June', '2020'),
+(4, 'Computer 4', '100000', '25/06/2020', 'June', '2020'),
+(6, 'Computer 5', '2500000', '25/06/2020', 'June', '2020'),
+(7, 'Computer 6', '8500', '25/06/2020', 'June', '2020'),
+(8, 'Computer 7', '100000', '25/06/2020', 'June', '2020'),
+(9, 'Computer 8', '100000', '25/06/2020', 'June', '2020'),
+(10, 'Computer 10', '100000', '25/06/2020', 'June', '2020'),
+(13, 'dgsdf', '95209520', '25/06/20', 'June', '2020'),
+(14, 'fadf', '72000', '25/06/20', 'June', '2020'),
+(15, 'dsfdsf', '1540', '25/06/20', 'June', '2020'),
+(16, 'Mobile Phone', '25000', '26/06/20', 'June', '2020'),
+(17, 'Laptop', '50000', '26/06/20', 'June', '2020'),
+(18, 'Tv', '85200', '26/06/20', 'June', '2020'),
+(19, 'Monitor', '5000', '26/06/20', 'June', '2020'),
+(20, 'Ups', '9500', '26/06/20', 'June', '2020'),
+(21, 'keyboard', '200', '26/06/20', 'June', '2020'),
+(25, 'car', '250', '26/06/20', 'June', '2020'),
+(31, 'shirt', '500', '26/06/20', 'June', '2020'),
+(32, 'asfer', '2530', '26/06/20', 'June', '2020');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2020_06_25_181607_expenses_table', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `expenses`
+--
+ALTER TABLE `expenses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `expenses`
+--
+ALTER TABLE `expenses`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
